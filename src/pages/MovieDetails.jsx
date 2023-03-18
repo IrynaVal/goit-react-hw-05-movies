03a5676a5d803b6951e635dc9ef170786d2c4e8e
IrynaVal <piv8050@gmail.com>
2023-03-19 00:40:41 +0200

    add movieDetails+getFunctions

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,13 +1,68 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams, Outlet } from 'react-router-dom';
 import { getFilmById } from 'services/getFilms';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
-  const film = getFilmById(movieId);
+  // const film = getFilmById(movieId);
+  // console.log(film);
+  const [film, setFilm] = useState({});
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    getFilmById(movieId)
+      .then(data => {
+        console.log(data);
+        if (!data) {
+          return Promise.reject(new Error());
+        }
+        setFilm(data);
+        // console.log(film);
+      })
+      .catch(error => {
+        console.log('error');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [movieId]);
+  const poster = `https://image.tmdb.org/t/p/w780${film.poster_path}`;
+  const { title, original_title, release_date, vote_average, overview } = film;
+  // const releaseYear = release_date.slice(0, 4);
+  console.log(release_date);
   return (
     <>
-      <img></img>
-    <div>Now showing product with id - {movieId}</div>)</>
+      <div className="movie__container">
+        <div className="movie__img">
+          <img
+            src={poster}
+            alt={title}
+            className="movie__poster"
+            // onError="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/488px-No-Image-Placeholder.svg.png';"
+          />
+        </div>
+        <div className="movie__text">
+          <h2 className="movie__name">
+            {original_title}({release_date})
+          </h2>
+          <p>User score: {(vote_average * 10).toFixed(0)}%</p>
+
+          <h3 className="movie__about">Overview</h3>
+          <p className="movie__film-about">{overview}</p>
+
+          <h4>Genres</h4>
+          <p>
+            {/* {genres.map(genre => {
+              return genre.name + '  ';
+            })} */}
+          </p>
+        </div>
+      </div>
+      <div>Now showing product with id - {movieId}</div>
+      <Outlet />
+      {loading && <p>Loading...</p>}
+    </>
+  );
 };
 
 export default MovieDetails;
--- src/services/getFilms.js
+++ src/services/getFilms.js
@@ -19,6 +19,31 @@ export async function getFilmById(filmId = 504949) {
   console.log(response.data);
   return response.data;
 }
+
+export async function getFilmByQuery(query = 'batman') {
+  const response = await axios.get(
+    `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+    // search/movie?api_key=${TOKEN}&language=en-US&&page=${params[1]}&include_adult=false
+  );
+  console.log(response.data);
+  return response.data;
+}
+
+export async function getFilmCast() {
+  const response = await axios.get(
+    `${BASE_URL}movie/{movie_id}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  console.log(response.data);
+  return response.data;
+}
+
+export async function getFilmReviews() {
+  const response = await axios.get(
+    `${BASE_URL}movie/{movie_id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  console.log(response.data);
+  return response.data;
+}
 // API Key: 4b190e449f27cff49b7be3cb0384f9d7
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>       trends
